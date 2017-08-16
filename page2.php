fab57e258221476016c27a04a0f0ca7b7cf6617d
guhdias <guhdias@me.com>
2017-08-15 22:39:39 -0300

    .

--- page2.php
+++ page2.php
@@ -14,25 +14,28 @@
 			<form action="page2.php" method="post" class="testes">
 				<table class="testes_tabela">
 					<tr>
-						<td colspan=4 class="testes_titulo">Digite o texto abaixo:</td>
+						<td colspan=7 class="testes_titulo">Digite o texto abaixo:</td>
 					</tr>
 					<tr>
-						<td colspan=4 class="testes_imagem"><img alt="Captcha Texto 1" src="images/text_captcha/text_captcha.jpg" width=100%></td>
+						<td colspan=7 class="testes_imagem"><img alt="Captcha Texto 1" src="images/text_captcha/text_captcha.jpg" class="teste_texto"></td>
 					</tr>
 					<tr>
-						<td colspan=4 class="testes_resposta"><input type="text" placeholder="texto" name="respTextCaptcha1" /></td>
+						<td colspan=7 class="testes_resposta"><input type="text" placeholder="texto" name="respTextCaptcha1" /></td>
 					</tr>
 					<tr class="testes_botoes">
-						<td colspan=2 class="testes_pular"><button>Pular</button></td>
-						<td colspan=2 class="testes_enviar"><button>Enviar</button></td>
+						<td colspan=3 class="testes_pular"><button>Pular</button></td>
+						<td colspan=4 class="testes_enviar"><button>Enviar</button></td>
 					</tr>
 					<tr>
-						<td colspan=4 class="testes_progresso"><img alt="Progresso 1/5" src="https://css-tricks.com/wp-content/uploads/2013/08/reset-progress-bar.png" width=80%></td>
+						<td colspan=7 class="testes_progresso"><img alt="Progresso 1/5" src="images/progress_bar/progress_bar_01.png"></td>
 					</tr>
 					<tr class="testes_abas">
 						<td class="testes_aba1">Tipo 1</td>
+						<td class="testes_aba_space">&nbsp;</td>
 						<td class="testes_aba2">Tipo 2</td>
+						<td class="testes_aba_space">&nbsp;</td>
 						<td class="testes_aba3">Tipo 3</td>
+						<td class="testes_aba_space">&nbsp;</td>
 						<td class="testes_aba4">Tipo 4</td>
 					</tr>
 				</table> 
