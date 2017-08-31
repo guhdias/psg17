45df76ac32e678f53d1c08179a03f3bb755bbe0c
guhdias <guhdias@me.com>
2017-08-30 22:50:56 -0300

    .

--- Copy of index.php
+++
@@ -1,9 +0,0 @@
-<html> 
-    <body> 
-        <form action="page2.php" method="post"> 
-            Test : <input type="text" name="name" size="40" length="40" value="test"><BR> 
-            <input type="submit" name="submit" value="Submit"> 
-            <input type="reset" name="reset" value="Clear It"> 
-        </form> 
-    </body> 
-</html> 
