1c7d89b940aaeb7e5faf250bcba123d4f8c9e38d
guhdias <guhdias@me.com>
2017-10-16 22:17:24 -0200

    .

--- index.php
+++ index.php
@@ -47,15 +47,15 @@ if (!($detect->isMobile())) {
 					identificar se o usuário é um computador ou um ser humano, são
 					mundialmente utilizados nos dias de hoje.</p>
 
-				<p class="corpo">Este estudo visa identificar a implementação de um
-					teste CAPTCHA em celulares que proporciona a melhor experiência
+				<p class="corpo">Este estudo visa identificar qual a implementação de
+					testes CAPTCHA em celulares que proporciona a melhor experiência
 					para o usuário.</p>
 
 				<p class="corpo">Você realizará 4 tipos diferentes de testes,
 					respondendo cada um deles 5 vezes e avaliando cada um deles no
 					final de acordo com sua experiência.</p>
 
-				<p class="corpo">Esta avaliação tem duração estimada de X minutos.</p>
+				<p class="corpo">Esta avaliação tem duração estimada de 5 minutos.</p>
 
 				<input type="hidden" id="tempo_gasto" name="tempo_gasto" value="" />
 				<input type="hidden" id="data_hora" name="data_hora" value="" />
