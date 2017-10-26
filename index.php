1710ba4a8bbe3b02657ccd9bd89fcd1500f98284
guhdias <guhdias@me.com>
2017-10-26 21:10:03 -0200

    GA

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ if (!($detect->isMobile())) {
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108707803-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108707803-1');
+</script>
 <meta charset="UTF-8">
 <title>Pesquisa testes CAPTCHA</title>
 
